ecaf157273d284ef1232d5c2119b9c9a1ca7eeeb
Peter Buskens <croak.croak929@gmail.com>
2018-04-16 02:48:07 +0000

    user show view & system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,24 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:show]
   
   def show
-    @user = User.find(params[:id])
+    if params[:search_date] != nil
+      puts "********debug*********"
+      unless params[:search_date][:"datecost(1i)"].empty?
+        date = Date.new(params[:search_date][:"datecost(1i)"].to_i, params[:search_date][:"datecost(2i)"].to_i, params[:search_date][:"datecost(3i)"].to_i) 
+        @date = date
+        @datecost = current_user.datecost(date)
+      end
+      unless params[:search_date][:"monthcost(1i)"].empty?
+        month = Date.new(params[:search_date][:"monthcost(1i)"].to_i, params[:search_date][:"monthcost(2i)"].to_i, 1)
+        @month = month
+        @monthcost = current_user.monthcost(month)
+      end
+      unless params[:search_date][:"yearcost(1i)"].empty?
+        year = Date.new(params[:search_date][:"yearcost(1i)"].to_i, 1, 1)
+        @year = year
+        @yearcost = current_user.yearcost(year)
+      end
+    end
   end
 
   def new
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,30 @@
 module UsersHelper
+  def allCost()
+    user = current_user
+    cost = 0
+    menu = Array.new
+
+    menu = user.usermenus.all
+    menu.each do |recipe|
+      id = recipe.recipe_id
+      cost += Recipe.find(id).cost
+    end
+    
+    cost 
+  end
+  
+  def weekCost()
+    user = current_user
+    cost = 0
+    menu = Array.new
+
+    menu = user.usermenus.all
+    menu.each do |recipe|
+      id = recipe.recipe_id
+      cost += Recipe.find(id).cost
+    end
+    
+    cost 
+  end
+  
 end
